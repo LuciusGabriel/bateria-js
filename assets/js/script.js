47d34086baaecc9f5f8e76628d247febd831e46c
LuciusGabriel <lucius_gabriel@hotmail.com>
2023-04-29 23:14:56 -0300

    Primeiros Sons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,12 @@
-document.body.addEventListener{'keyup', (event)=>{
-    console.log(event.code)
+document.body.addEventListener('keyup', (event)=>{
+    playSound(event.code.toLowerCase())
 }
+)
+
+function playSound(sound){
+    let audioElement = document.querySelector(`#s_${sound}`)
+
+    if(audioElement){
+        audioElement.play();
+    }
 }
